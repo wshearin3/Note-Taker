9d9a52f5af9e1941a11466b2ae51e4b041de84ad
Trey <trey.shearin3@gmail.com>
2024-04-17 11:34:20 -0400

    changed relative file paths for routes on server.js get routes

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const app = express();
 const PORT = process.envPORT || 3008;
 
 app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, 'index.html'))
-)
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, 'notes.html'))
-)
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
 app.listen(PORT, () =>
 console.log(`app listenting at http://localhost:${PORT}`)
-)
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
