210a2da3065285d151b64e5b48673585dc271e06
Trey <trey.shearin3@gmail.com>
2024-04-17 11:52:04 -0400

    updated get request in apiRoutes to use utils functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,39 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const notes = require('express').Router();
 const { json } = require('express');
-const {readFromFile, readAndAppend,writeToFile} = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 const dbPath = path.join(__dirname, './db/db.json');
 
-router.get('/notes', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Failed to read notes.' });
-    }
-    res.json(JSON.parse(data));
-  });
+notes.get('/', (req, res) => {
+
+    readFromFile(dbPath).then((data) => res.join(JSON.parse(data)));
+
 });
 
 router.post('/notes', (req, res) => {
     const newNote = req.body;
-    newNote.id= Math.floor((1 + Math.random())*0x10000).toString(16).substring(1);
-  
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Failed to read notes.' });
-    }
-    const notes = JSON.parse(data);
-    notes.push(newNote);
-    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Failed to save note.' });
-      }
-      res.json(newNote);
+    newNote.id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes.' });
+        }
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to save note.' });
+            }
+            res.json(newNote);
+        });
     });
-  });
 });
 
 
