00ca89a516abffd216d04939e2fdaa4e3440340f
Trey <trey.shearin3@gmail.com>
2024-04-17 11:42:47 -0400

    set query selectors for /notes page elements

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,3 +4,13 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
+
+if (window.location.pathname === '/notes') {
+    noteForm = document.querySelector('.note-form');
+    noteTitle = document.querySelector('.note-title');
+    noteText = document.querySelector('.note-textarea');
+    saveNoteBtn = document.querySelector('.save-note');
+    newNoteBtn = document.querySelector('.new-note');
+    clearBtn = document.querySelector('.clear-btn');
+    noteList = document.querySelectorAll('.list-container .list-group');
+  }
