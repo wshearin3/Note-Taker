d371ca58bbb5746a67213e4d15ae966be26cafaa
Trey <trey.shearin3@gmail.com>
2024-04-17 11:38:08 -0400

    created writeToFile function in utils file

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,2 +1,9 @@
 const fs = require('fs');
 const util = require('util');
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
