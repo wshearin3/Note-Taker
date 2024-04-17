64fb8214c021f9efd3a6c9b1b20c6b96e919f676
Trey <trey.shearin3@gmail.com>
2024-04-16 20:06:01 -0400

    created routes for index and notes html files

--- server.js
+++ server.js
@@ -2,3 +2,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const PORT = process.envPORT || 3008;
+
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, 'index.html'))
+)
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'notes.html'))
+)
+
+app.listen(PORT, () =>
+console.log(`app listenting at http://localhost:${PORT}`)
+)
